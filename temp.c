a5455adc928a00c28ee3f07e763c7fddd3dd95cd
Avocado_bear <529963563@qq.com>
2023-10-25 00:50:39 +0800

    lock finished

---
+++ temp.c
@@ -0,0 +1,78 @@
+static struct buf*
+bget(uint dev, uint blockno) {
+  struct buf* b;
+
+  int bid = HASH(blockno);
+  acquire(&bcache.buckets[bid].lock);
+
+  // Is the block already cached?
+  for(b = bcache.buckets[bid].head.next; b != &bcache.buckets[bid].head; b = b->next) {
+    if(b->dev == dev && b->blockno == blockno) {
+      b->refcnt++;
+
+      // 记录使用时间戳
+      acquire(&tickslock);
+      b->timestamp = ticks;
+      release(&tickslock);
+
+      release(&bcache.buckets[bid].lock);
+      acquiresleep(&b->lock);
+      return b;
+    }
+  }
+
+  // Not cached.
+  b = 0;
+  struct buf* tmp;
+
+  // Recycle the least recently used (LRU) unused buffer.
+  // 从当前散列桶开始查找
+  for(int i = bid, cycle = 0; cycle != NBUCKET; i = (i + 1) % NBUCKET) {
+    ++cycle;
+    // 如果遍历到当前散列桶，则不重新获取锁
+    if(i != bid) {
+      if(!holding(&bcache.buckets[i].lock))
+        acquire(&bcache.buckets[i].lock);
+      else
+        continue;
+    }
+
+    for(tmp = bcache.buckets[i].head.next; tmp != &bcache.buckets[i].head; tmp = tmp->next)
+      // 使用时间戳进行LRU算法，而不是根据结点在链表中的位置
+      if(tmp->refcnt == 0 && (b == 0 || tmp->timestamp < b->timestamp))
+        b = tmp;
+
+    if(b) {
+      // 如果是从其他散列桶窃取的，则将其以头插法插入到当前桶
+      if(i != bid) {
+        b->next->prev = b->prev;
+        b->prev->next = b->next;
+        release(&bcache.buckets[i].lock);
+
+        b->next = bcache.buckets[bid].head.next;
+        b->prev = &bcache.buckets[bid].head;
+        bcache.buckets[bid].head.next->prev = b;
+        bcache.buckets[bid].head.next = b;
+      }
+
+      b->dev = dev;
+      b->blockno = blockno;
+      b->valid = 0;
+      b->refcnt = 1;
+
+      acquire(&tickslock);
+      b->timestamp = ticks;
+      release(&tickslock);
+
+      release(&bcache.buckets[bid].lock);
+      acquiresleep(&b->lock);
+      return b;
+    } else {
+      // 在当前散列桶中未找到，则直接释放锁
+      if(i != bid)
+        release(&bcache.buckets[i].lock);
+    }
+  }
+
+  panic("bget: no buffers");
+}
